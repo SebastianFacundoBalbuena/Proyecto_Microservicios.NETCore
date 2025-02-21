6dbd0b98f5ddd0ce15120c845eb2704555ec2638
SebastianFB <sebastianfacundobalbuena@gmai.com>
2025-02-21 20:19:20 -0300

    Actualizacion dia 21-02-2025

---
+++ Koditi.Ecommerce/src/Services/Customer/Customer.Testing/CustomerServicesEventHandlerTest.cs
@@ -0,0 +1,61 @@
+﻿
+using Customer.Testing.DataBaseTesting;
+using Customer.Services.EventHandlders;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Customers.Services.EventHandlders.Command;
+
+namespace Customer.Testing
+{
+    [TestClass]
+    public class CustomerServicesEventHandlerTest
+    {
+        private ILogger<CustomerServicesEventHandler> _logger
+        {
+            get
+            {
+                return new Mock<ILogger<CustomerServicesEventHandler>>().Object;
+            }
+        }
+
+        [TestMethod]
+        public void CreateNewCustomer()
+        {
+            var context = ApplicationDbContextCustomerInMemory.Get();
+
+            CustomerCommandEventHandler customer = new CustomerCommandEventHandler()
+            {
+                Name = "Test",
+            };
+
+            var handler = new CustomerServicesEventHandler(context, _logger);
+
+            handler.Handle(customer, new CancellationToken()).Wait();
+        }
+
+        [TestMethod]
+        public void UpdateCustomer()
+        {
+            var context = ApplicationDbContextCustomerInMemory.Get();
+
+            Domain.Customer customer = new Domain.Customer()
+            {
+                Name = "Test2"
+            };
+
+            context.Customers.AddAsync(customer);
+            context.SaveChangesAsync();
+
+            CustomerCommandEventHandler UpdateCustomer = new CustomerCommandEventHandler()
+            {
+                customerId = customer.CustomerId,
+                Name = "TestActualizado",
+            };
+
+            var handler = new CustomerServicesEventHandler(context, _logger);
+
+            handler.Handle(UpdateCustomer, new CancellationToken()).Wait();
+
+        }
+    }
+}
---
+++ Koditi.Ecommerce/src/Services/Customer/Customer.Testing/DataBaseTesting/ApplicationDbContextCustomerInMemory.cs
@@ -0,0 +1,16 @@
+﻿using Customer.Persistence.DataBase;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customer.Testing.DataBaseTesting
+{
+    public class ApplicationDbContextCustomerInMemory
+    {
+        public static ApplicationDbContextCustomer Get()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContextCustomer>()
+                .UseInMemoryDatabase(databaseName: "Customer.DataBase").Options;
+
+            return new ApplicationDbContextCustomer(options);
+        }
+    }
+}
